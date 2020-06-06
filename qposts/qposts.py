ef4ca5aa77b5894c0536f5477893c5bde36d1616
learnedtocode <66190005+learnedtocode@users.noreply.github.com>
2020-06-06 07:51:30 +0000

    try fix

--- qposts/qposts.py
+++ qposts/qposts.py
@@ -449,6 +449,5 @@ class QPosts(getattr(commands, "Cog", object)):
 
     def __unload(self):
         self.bot.loop.create_task(self.session.close())
-        self.loop.cancel()
 
     __del__ = __unload
